54be674487b3ac5db7700c9274b1431e0cdfb085
WLaffet <wlaffet@wlaffet-virtual-machine.(none)>
2012-10-23 09:48:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PointageRails::Application.config.secret_token = '2b9c066fa7387953e647686906e19594e8795bcfc14a2c2e7a9ffee69b92e83642f06bd1e2596199f08a40a428e45009e5e2d98c638c915576291aac90ccaa5f'
